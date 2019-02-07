0089e932b6d5812f92e86e18d22b91e74ed68600
Paulo Ferreira <Paulo.Ferreira@saiglobal.com>
2019-02-07 12:38:48 -0200

    Implement filters and styles

--- i18n-helper.js
+++ i18n-helper.js
@@ -2864,26 +2864,49 @@
         },
     };
 
+    const clearRootClass = () => {
+        const langsRoot = document.querySelectorAll('#content > .parent');
+        const arrLangs = Array.from(langsRoot);
+
+        return arrLangs.map(lang => document.querySelector(`#${lang.id}`).className = '');
+    };
+
+    const countStrings = (elements, isFiltered = false) => {
+        return elements.map(el => {
+            const count = document.querySelector(el).querySelectorAll('div').length;
+
+            if (isFiltered) {
+                return document.querySelector(`${el} > strong > span`).innerHTML = ` (${count - 1})`;
+            }
+
+            return document.querySelector(`${el} > strong`).innerHTML += ` (${count})`;
+        });
+    };
+
     const verifyTextIntegrity = (string, type = 'parent') => {
         const element = document.querySelectorAll(`div[data-text="${string}"]`);
         const arrElement = Array.from(element);
 
         if (arrElement.length % 4 === 0) {
             return arrElement.map(el => document.querySelector(`#${el.id}`).className = `${type} ok`);
-        } else if (arrElement.length % 4 !== 0) {
-            return arrElement.map(el => document.querySelector(`#${el.id}`).className = `${type} partial`);
+        } else if (arrElement.length === 1) {
+            return arrElement.map(el => document.querySelector(`#${el.id}`).className = `${type} nok`);
         }
 
-        return arrElement.map(el => document.querySelector(`#${el.id}`).className = `${type} nok`);
+        return arrElement.map(el => document.querySelector(`#${el.id}`).className = `${type} partial`);
     };
 
-    const renderLevel = (nodes, element) => (
-        Object.keys(nodes).map((content, key) => {
+    const renderLevel = (nodes, element, isRoot = false, currentLang = 'en-US') => {
+        return Object.keys(nodes).map((content, key) => {
+            if (isRoot) {
+                currentLang = content;
+            }
+
             if (nodes[content] && typeof nodes[content] === 'object') {
                 element.innerHTML += `
                 <div
                     data-text="${content}"
-                    class="parent"
+                    data-lang="${currentLang}"
                     id="${element.id}-${content}-${key}"
                 >
                     <strong>${content}:</strong>
@@ -2891,13 +2914,13 @@
             `;
 
                 verifyTextIntegrity(content, 'parent');
-                return renderLevel(nodes[content], document.getElementById(`${element.id}-${content}-${key}`));
+                return renderLevel(nodes[content], document.getElementById(`${element.id}-${content}-${key}`), false, currentLang);
             }
 
             element.innerHTML += `
             <div
                 data-text="${content}"
-                class="node"
+                data-lang="${currentLang}"
                 id="${element.id}-${content}-${key}"
             >
                 <strong>${content}:</strong> ${nodes[content]}
@@ -2906,9 +2929,110 @@
 
             return verifyTextIntegrity(content, 'node');
         })
-    );
+    };
 
     const contentWrapper = document.getElementById('content');
-    renderLevel(translations, contentWrapper);
+    renderLevel(translations, contentWrapper, true);
+    clearRootClass();
+    countStrings(['div[data-text=en-US]', 'div[data-text=pt-BR]', 'div[data-text=es-ES]', 'div[data-text=de-DE]']);
+
+    // Filters
+    document.getElementById('filterByNOK').addEventListener('click', () => {
+        const enWrapper = document.getElementById('en-filter');
+        const ptWrapper = document.getElementById('pt-filter');
+        const esWrapper = document.getElementById('es-filter');
+        const deWrapper = document.getElementById('de-filter');
+
+        [enWrapper, ptWrapper, esWrapper, deWrapper].map(lang => lang.innerHTML = '');
+
+        document.getElementById('filterByPartial').classList = '';
+        document.getElementById('filteredWrapper').classList = '';
+        document.getElementById('clearFilter').classList = '';
+
+        const nok = document.querySelectorAll('.nok');
+        const arrNOK = Array.from(nok);
+
+        document.getElementById('content').classList = 'hidden';
+        document.getElementById('filterByNOK').classList = 'active';
+
+        arrNOK.map(el => {
+            const cloneEl = el.cloneNode(true);
+            
+            switch(cloneEl.attributes['data-lang'].value) {
+                case 'en-US':
+                    return enWrapper.append(cloneEl)
+                case 'pt-BR':
+                    return ptWrapper.append(cloneEl)
+                case 'es-ES':
+                    return esWrapper.append(cloneEl)
+                case 'de-DE':
+                    return deWrapper.append(cloneEl)
+            }
+        });
+
+        return countStrings([
+            'div[data-filtered-text=en-US]',
+            'div[data-filtered-text=pt-BR]',
+            'div[data-filtered-text=es-ES]',
+            'div[data-filtered-text=de-DE]'
+        ], true);
+    });
+
+    document.getElementById('filterByPartial').addEventListener('click', () => {
+        const enWrapper = document.getElementById('en-filter');
+        const ptWrapper = document.getElementById('pt-filter');
+        const esWrapper = document.getElementById('es-filter');
+        const deWrapper = document.getElementById('de-filter');
+
+        [enWrapper, ptWrapper, esWrapper, deWrapper].map(lang => lang.innerHTML = '');
+
+        document.getElementById('filterByNOK').classList = '';
+        document.getElementById('filteredWrapper').classList = '';
+        document.getElementById('clearFilter').classList = '';
+
+        const nok = document.querySelectorAll('.partial');
+        const arrNOK = Array.from(nok);
+
+        document.getElementById('content').classList = 'hidden';
+        document.getElementById('filterByPartial').classList = 'active';
+
+        arrNOK.map(el => {
+            const cloneEl = el.cloneNode(true);
+            
+            switch(cloneEl.attributes['data-lang'].value) {
+                case 'en-US':
+                    return enWrapper.append(cloneEl)
+                case 'pt-BR':
+                    return ptWrapper.append(cloneEl)
+                case 'es-ES':
+                    return esWrapper.append(cloneEl)
+                case 'de-DE':
+                    return deWrapper.append(cloneEl)
+            }
+        });
+
+        return countStrings([
+            'div[data-filtered-text=en-US]',
+            'div[data-filtered-text=pt-BR]',
+            'div[data-filtered-text=es-ES]',
+            'div[data-filtered-text=de-DE]'
+        ], true);
+    });
+
+    document.getElementById('clearFilter').addEventListener('click', () => {
+        document.getElementById('filterByNOK').classList = '';
+        document.getElementById('filterByPartial').classList = '';
+        document.getElementById('filteredWrapper').classList = 'hidden';
+        document.getElementById('content').classList = '';
+        document.getElementById('clearFilter').classList = 'disabled';
+
+        const enWrapper = document.getElementById('en-filter');
+        const ptWrapper = document.getElementById('pt-filter');
+        const esWrapper = document.getElementById('es-filter');
+        const deWrapper = document.getElementById('de-filter');
+
+        [enWrapper, ptWrapper, esWrapper, deWrapper].map(lang => lang.innerHTML = '');
+    });
+    // Filters End
 
 }());
